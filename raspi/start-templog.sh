c61438b49bbc8723b0d59b70839b49591dfe6bdb
datdenkikniet <jcdra1@gmail.com>
2020-11-22 14:04:04 +0100

    Update

--- raspi/start-templog.sh
+++ raspi/start-templog.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
+
+# Don't forget to set GRAFANA_USER, GRAFANA_PASSWORD and CHIPNAME in .screenrc
+
 echo "none" > /sys/class/leds/led0/trigger
 /root/enable-rtc.sh
 hwclock -s
 /root/disable-rtc.sh
 touch /run/currenttemp.txt
 chmod 644 /run/currenttemp.txt
-screen -S temp-logger -d -m /root/log-temp.sh
+screen -S temp-logger -d -m "/root/log-temp.sh"
